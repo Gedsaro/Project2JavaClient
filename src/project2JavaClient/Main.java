4e83102e5ee7b41ce0c5dfa5b14da00f87933bf7
mnf436 <mnf436@PEON2.usask.ca>
2017-10-19 19:28:20 -0600

    Java Client test
    Made a simple java client at uni,
    single theread, blocking, can basic text line talk to the java server.
    can also do basic readline text talking to the c# server in the same way

--- src/project2JavaClient/Main.java
+++ src/project2JavaClient/Main.java
@@ -7,28 +7,46 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) throws UnknownHostException, IOException {
-		String hostName = "";
+		String hostName = "localhost";
 		int portNumber = 8000;
 		String fromServer = "";
 		String fromUser = "";
+		
+		System.out.println("Begining");
 		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("buffered reader stdIn set up");
 
 		Socket kkSocket = new Socket(hostName, portNumber);
+		System.out.println("Connected?");
 		PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
+		System.out.println("Made printwriter uising socket outputstream");
 		BufferedReader in = new BufferedReader(new InputStreamReader(kkSocket.getInputStream()));
-
+		System.out.println("Made beffered reader out of in from inputsreamreader for socket");
+		
+		System.out.println("Please type something");
+		out.println("I AM A TEST");
+		//fromUser = stdIn.readLine();
+		//System.out.println("You have typed: " + fromUser);
+		
+		//out.println(fromUser);
 		
 		while((fromServer = in.readLine()) != null) {
+
 			System.out.println("Server: " + fromServer);
+
 			if(fromServer.equals("Bye."))
+			{
+				System.out.println("fromServer = " + fromServer);
 				break;
+			}
 			
 			fromUser = stdIn.readLine();
 			if(fromUser != null) {
 				System.out.println("Client: " + fromUser);
 				out.println(fromUser);
-			
 			}
+			
+			
 		}
 	}
 
